d4201a828422f6362e12297377f3774d5491567b
sam-iv <samivuerah@gmail.com>
2022-10-13 12:18:02 +0100

    Exercise 3 completed

---
+++ Lab03/src/main/java/lab3_exercise3/AnnualSalary.java
@@ -0,0 +1,38 @@
+package lab3_exercise3;
+
+public class AnnualSalary {
+	private double salary;
+	private static final int allowance = 12500;
+	private static final double bRate = 0.2;
+	private static final double hRate = 0.4;
+	private static final int taxThreshold = 50000;
+	private static final int maxTax = 7500;
+	
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public AnnualSalary() {
+		super();
+	}
+	
+	public double calculateTax() {
+		if (salary > allowance && salary <= taxThreshold) {
+			double taxSalary = (salary - allowance);
+			taxSalary *= bRate;
+			
+			return(taxSalary);
+		}
+		if (salary > taxThreshold) {
+			double taxSalary = (salary - taxThreshold);
+			taxSalary *= hRate;
+			
+			return(taxSalary + maxTax);
+		} else {
+			return 0D;
+		}
+	}
+}
---
+++ Lab03/src/main/java/lab3_exercise3/Employee.java
@@ -0,0 +1,56 @@
+package lab3_exercise3;
+
+public class Employee {
+	private int id;
+	private String forename, surname;
+	private AnnualSalary salary;
+	private Position companyPosition;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getForename() {
+		return forename;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public double getSalary() {
+		return salary.getSalary();
+	}
+	public String getPositionName() {
+		return companyPosition.getRoleName();
+	}
+
+	public Employee(int id, String forename, String surname, AnnualSalary salary, Position companyPosition) {
+		super();
+		this.id = id;
+		this.forename = forename;
+		this.surname = surname;
+		this.salary = salary;
+		this.companyPosition = companyPosition;
+	}
+	
+	public void displayEmployeeName() {
+		System.out.println(forename + " " + surname);
+	}
+	
+	private boolean eligibleForBonus() {
+		if (salary.getSalary() >= 40000) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	@Override
+	public String toString() {
+		if (eligibleForBonus() == true) {
+			return surname + ", " + forename + " (" + id + "): " + getPositionName() + " at £" + salary.getSalary() + 
+					" (£" + salary.calculateTax() + " tax) and is eligible for bonus.";
+		} else {
+			return surname + ", " + forename + " (" + id + "): " + getPositionName() + " at £" + salary.getSalary() + 
+					" (£" + salary.calculateTax() + " tax) and is not eligible for bonus.";
+		}
+	}
+}
