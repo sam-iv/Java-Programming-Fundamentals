748d6ca73a9ee162d14ffc2c4a4baa435e4d90dd
sam-iv <samivuerah@gmail.com>
2022-11-06 01:33:02 +0000

    Lab06 complete

--- Lab06/src/main/java/lab6_exercise3/MedalTable.java
+++ Lab06/src/main/java/lab6_exercise3/MedalTable.java
@@ -9,8 +9,22 @@ public class MedalTable {
 	}
 	
 	public void addMedal(Countries countries, Medals medals) {
-		/*for (Countries country : ) {
-			
-		}*/
+		this.medals[countries.getIndex() - 1][medals.getIndex()] += 1;
 	}
+	
+	public int getEUMedalCount() {
+		int medalCount = 0;
+		for (Countries country : Countries.values()) {
+			if (country.isEU()) {
+				for (int i = 0; i < 3; i++) {
+					medalCount += this.medals[country.getIndex() - 1][i];
+				}
+			}
+		}
+		return medalCount;
+	}
+	
+	 public int getMedals(Countries countries, Medals medals) {
+		 return this.medals[countries.getIndex() - 1][medals.getIndex()];
+	 }
 }
